686c5d724418dbe103fa01ed2bdd490f4ade4634
oguzhan7576 <61021720+oguzhan7576@users.noreply.github.com>
2020-05-17 01:18:59 +0200

    d
    d

---
+++ commands/close.js
@@ -0,0 +1,35 @@
+const discord = require("discord.js");
+ 
+module.exports.run = async (bot, message, args) => {
+ 
+    // Id van category van tickets.
+    const categoryId = "498092615351271425";
+ 
+    // Als bericht in ticket kanaal is dan verwijder kanaal ander zend bericht
+    if (message.channel.parentID == categoryId) {
+ 
+        message.channel.delete();
+ 
+    } else {
+ 
+        message.channel.send("Gelieve dit commando in een ticket kanaal te doen.");
+ 
+    }
+ 
+    var embedCloseTicket = new discord.RichEmbed()
+        .setTitle("Hoi, " + message.channel.name)
+        .setDescription("Je ticket is gemarkeerd als **compleet**. Wil je een nieuwe maken doe dan !ticket")
+        .setFooter("ticket gesloten");
+ 
+    // Vind kanaal voor de logs.
+    var logChannel = message.guild.channels.find("name", "log");
+    if (!logChannel) return message.channel.send("Kanaal bestaat niet");
+ 
+    logChannel.send(embedCloseTicket);
+ 
+}
+ 
+module.exports.help = {
+    name: "close",
+    description: "Sluit een ticket af"
+}
