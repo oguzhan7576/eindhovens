bccc138657d824ed59cacc49901ad4608f5d2da4
oguzhan7576 <61021720+oguzhan7576@users.noreply.github.com>
2020-05-17 01:03:35 +0200

    gf
    fx

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 const discord = require("discord.js");
 const BotConfig = require("./botconfig.json");
 
+
+const fs = require("fs");
+
+
 const bot = new discord.Client();
+
+fs.readdir("./commands/", (err, files) => {
+    if(err) console.log(err);
+
+    var jsFiles = files.filter(f => f.split(".").pop() === "js");
+
+ if (jsFiles.length <= 0) {
+     
+    console.log("kon geen files vinden");
+     return; 
+ }
+
+jsFiles.forEach((f, i) => {
+
+    var fileGet = require(`./commands/${f}`);
+    console.log(`De file ${f} is geladen`);
+
+    bot.commands.set(fileGet.help.name, fileGet);
+})
+
+
+});
 
 
 bot.on("ready", async () => {
@@ -25,6 +51,12 @@ bot.on("message", async message => {
    var command = messageArray[0];
 
    var arguments = messageArray.slice(1);
+
+var commands = bot.commands.get(command.slice(prefix.length));
+
+if(commands) commands.run(bot, message, arguments);
+
+
 
    if(command === `${prefix}ip`){
 
