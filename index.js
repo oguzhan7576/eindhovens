f553497188f83cc42d59bd7fa7a8fe0ec61ad0be
oguzhan7576 <61021720+oguzhan7576@users.noreply.github.com>
2020-05-17 00:17:26 +0200

    init
    init

---
+++ index.js
@@ -0,0 +1,46 @@
+const discord = require("discord.js");
+const botConfig = require("./botConfig.json");
+
+const bot = new discord.Client();
+
+
+bot.on("ready", async () => {
+
+    console.log(`${bot.user.username} is online`);
+
+    bot.user.setActivity("Eindhoven Roleplay", { type: "PLAYING" });
+
+});
+
+bot.on("message", async message => {
+
+    if(message.author.bot) return;
+  
+   if(message.channel.type === "dm") return; 
+
+   var prefix = BotConfig.prefix;
+
+   var messageArray = message.content.split(" "); 
+
+   var command = messageArray[0];
+
+   var arguments = messageArray.slice(1);
+
+   if(command === `${prefix}ip`){
+
+    return message.channel.send("**IP**:`cfx.re/join/3yb8qr`");
+
+   }
+
+   
+   if(command === `${prefix}citybug`){
+
+    return message.channel.send("Probeer deze grafische instellingen: || https://imgur.com/eFKbcKR ||");
+
+   }
+
+  
+
+});
+
+bot.login(process.env.token);
