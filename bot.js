9ec201cadec8fc8aa7942d56a008dacf25898717
oguzhan7576 <61021720+oguzhan7576@users.noreply.github.com>
2020-05-19 15:34:30 +0200

    dc
    d

--- bot.js
+++ bot.js
@@ -5,9 +5,9 @@ global.config = require("./config.json")
 // invite your bot
 // https://discordapp.com/oauth2/authorize?client_id=your_client_id&scope=bot&permissions=8
 client.on('ready', () => {
-    console.log(`Discord Bot logged in as ${client.user.tag}`);
+    console.log(`Discord bot is ingelogt als: ${client.user.tag}`);
     updatePlayerCount(client, 5)
     //this will update the bot's activity every 5 seconds
 });
 
-bot.login(process.env.token);
+client.login(config.token);
