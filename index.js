7d5b088b4f95ce6e7abc8c9d1f8c75a1f22ca517
oguzhan7576 <61021720+oguzhan7576@users.noreply.github.com>
2020-05-19 15:25:47 +0200

    d

--- index.js
+++ index.js
@@ -1,82 +1,13 @@
-const discord = require("discord.js");
-const botConfig = require("./botconfig.json");
-
-
-const fs = require("fs");
-
-const bot = new discord.Client();
-bot.commands = new discord.Collection();
-
-fs.readdir("./commands/" , (err, files) => {
-
-if(err) console.log(err); 
-
-var jsFiles = files.filter(f => f.split(".").pop() === "js");
-
-if(jsFiles.length <= 0){
-    console.log("Kon geen files vinden probeer het opnieuw bro");
-    return; 
-}
-
-jsFiles.forEach((f,i) => {
-
-var fileGet = require(`./commands/${f}`);
-console.log(`De file ${f} is geladen`);
-
-bot.commands.set(fileGet.help.name, fileGet);
-
-
-})
-
+const { Client } = require('discord.js');
+const client = new Client;
+const {updatePlayerCount} = require("./utils/")
+global.config = require("./config.json")
+// invite your bot
+// https://discordapp.com/oauth2/authorize?client_id=your_client_id&scope=bot&permissions=8
+client.on('ready', () => {
+    console.log(`Discord Bot logged in as ${client.user.tag}`);
+    updatePlayerCount(client, 5)
+    //this will update the bot's activity every 5 seconds
 });
-
-bot.on("ready", async () => {
-
-console.log(`${bot.user.username} is online!`)
-
-bot.user.setActivity("HardCore Roleplay", {type: "PLAYING"});
-
-})
-
-//bericht
-bot.on("message", async message => {
-    
-    if(message.author.bot) return;
-
-if(message.channel.type === "dm") return;
-
-var prefix = botConfig.prefix;
-
-var messageArray = message.content.split(" ");
-
-var command = messageArray[0];
-
-var arguments = messageArray.slice(1);
-
-var commands = bot.commands.get(command.slice(prefix.length));
-
-if(commands) commands.run(bot,message, arguments);
-
-
-
-//Bercihtje 
-
-
-
-if(command === `${prefix}ip`){
-
-    return message.channel.send("*IP*: `cfx.re/join/3yb8qr` ");
-
-}
-
-if(command === `${prefix}citybug`){
-
-    return message.channel.send("Probeer deze grafische instellingen: || https://imgur.com/eFKbcKR ||");
-
-   }
- 
-});
-
-
 
 bot.login(process.env.token);
