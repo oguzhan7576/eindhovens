2ac64db50cfc2c501f661455297a1660de5366cc
oguzhan7576 <61021720+oguzhan7576@users.noreply.github.com>
2020-05-19 15:31:23 +0200

    Update index.js

--- utils/index.js
+++ utils/index.js
@@ -1,17 +1,7 @@
 module.exports = {
-    /**
-     * updatePlayerCount
-     * Constantly updates the player count of the server at a steady rate
-     * @param {object} client The client of the bot object
-     * @param {number} seconds The integer amount for the derivate of # of times it refreshes the bot's activity
-     * ```js
-     * const {updatePlayerCount} = require("./utils/")
-     * const { Client } = require('discord.js');
-     * const client = new Client;
-     * updatePlayerCount(client, 10)
-     * // this will update the bot's activity every 10 seconds
-     * ````
-     */
+ 
+    
+    
     updatePlayerCount: (client, seconds) => {
         const interval = setInterval(function setStatus() {
             status = `${GetNumPlayerIndices()} player(s)`
